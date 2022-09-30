40ea13df708d15da1ae276d0c9f9863fcd369bd3
ziwuling <ljw8009@gmail.com>
2022-09-30 02:59:48 +0000

    삼양식품js

---
+++ portfolio/slide02.js
@@ -0,0 +1,40 @@
+var swiper = new Swiper(".sec01", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".btn_pre",
+      prevEl: ".btn_next",
+    },
+    loop:true,
+    autoplay:{
+      delay:3000,
+    }
+  });
+  
+  $(function() {
+      // var isOn = false;
+      $(window).scroll(function() {
+          if ($(this).scrollTop() > 100) {
+              // if (!isOn) {
+                  $(".topbutton").addClass("on");
+                  // isOn = true;
+              // }
+          } else {
+              // if (isOn) {
+                  $(".topbutton").removeClass("on");
+                  // isOn = false;
+              // }
+          }
+      });
+      $(".topbutton").click(function() {
+          // $(window).scrollTop(0);
+          $("html, body").animate({
+              scrollTop: 0
+          },500);
+          // return false;
+          // event.preventDefault()
+      });
+  });
+  
