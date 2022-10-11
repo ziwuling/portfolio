83d5ba3fc6db066d762f715a93119105b1e61b3d
ziwuling <ljw8009@gmail.com>
2022-10-11 07:37:51 +0000

    수정

--- portfolio/삼양식품/nav연습.js
+++ portfolio/삼양식품/nav연습.js
@@ -1,39 +1,16 @@
-// $(function() {
-    // var iw = window.innerWidth;
-    // if (iw > 1080) {
-    //     $("#gnb .m").on("mouseenter focusin", function() {
-    //         $("ul", this).show(); // #gnb .m의 자식 ul
-    //         $(this).siblings("li").find("ul").hide();
-    //         $(this).addClass("on").siblings("li").removeClass("on");
-    //     }).on("mouseleave", function() {
-    //         $("#gnb ul").hide().add("#gnb .m").removeClass("on");            
-    //     });
-
-    // } else {
-    //     $("#gnb .m").on("click", function() {
-    //         $("ul", this).show(); // #gnb .m의 자식 ul
-    //         $(this).siblings("li").find("ul").hide();
-    //         $(this).addClass("on").siblings("li").removeClass("on");
-    //         return false;
-    //     });
-    // }
-    // $("*:not('#gnb a')").on("focus", function() {
-    //     // 모든객체 중 #gnb a가 아닌 요소들에 포커스가 들어오면  => nav를 벗어나면
-    //     $("#gnb ul").hide().add("#gnb .m").removeClass("on");
-    // });
-
-    // 기존소스
-//     $("#gnb .m").on("mouseenter focusin", function() {
-//         $("ul", this).show(); // #gnb .m의 자식 ul
-//         $(this).siblings("li").find("ul").hide();
-//         $(this).addClass("on").siblings("li").removeClass("on");
-//     }).on("mouseleave", function() {
-//         $("#gnb ul").hide().add("#gnb .m").removeClass("on");
-//         // $("#gnb ul").hide();     
-//         // $("#gnb .m").removeClass("on");             
-//     });
-//     $("*:not('#gnb a')").on("focus", function() {
-//         // 모든객체 중 #gnb a가 아닌 요소들에 포커스가 들어오면  => nav를 벗어나면
-//         $("#gnb ul").hide().add("#gnb .m").removeClass("on");
-//     });
-// });
+$(function() {
+    $(".gnb .m").on("mouseenter focusin", function() {
+        $("ul", this).show(); // #gnb .m의 자식 ul
+        $(this).siblings("li").find("ul").hide();
+        $(this).addClass("on").siblings("li").removeClass("on");
+    }).on("mouseleave", function() {
+        $(".gnb ul").hide().add(".gnb .m").removeClass("on");
+        // $("#gnb ul").hide();     
+        // $("#gnb .m").removeClass("on");             
+    });
+    $("*:not('.gnb a')").on("focus", function() {
+        // 모든객체 중 #gnb a가 아닌 요소들에 포커스가 들어오면  => nav를 벗어나면
+        $(".gnb ul").hide().add(".gnb .m").removeClass("on");
+    });
+});
+   
--- portfolio/삼양식품/samyangmain.js
+++ portfolio/삼양식품/samyangmain.js
@@ -1,3 +1,21 @@
+// header
+$(function() {
+  $(".gnb .m").on("mouseenter focusin", function() {
+      $("ul", this).show(); // #gnb .m의 자식 ul
+      $(this).siblings("li").find("ul").hide();
+      $(this).addClass("on").siblings("li").removeClass("on");
+  }).on("mouseleave", function() {
+      $(".gnb ul").hide().add(".gnb .m").removeClass("on");
+      // $("#gnb ul").hide();     
+      // $("#gnb .m").removeClass("on");             
+  });
+  $("*:not('.gnb a')").on("focus", function() {
+      // 모든객체 중 #gnb a가 아닌 요소들에 포커스가 들어오면  => nav를 벗어나면
+      $(".gnb ul").hide().add(".gnb .m").removeClass("on");
+  });
+});
+ 
+
 var sec01 = new Swiper(".sec01", {
   pagination: {
     el: ".sec01 .swiper-pagination",
