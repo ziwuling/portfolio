df86a9878d0ae30558324c3d9b9c84e72dfecc66
ziwuling <ljw8009@gmail.com>
2022-11-17 02:23:32 +0000

    직지페스티벌추가

---
+++ jikjifestival/jikjifestival.js
@@ -0,0 +1,37 @@
+var sec02slide = new Swiper(".sec02slide", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    slidesPerView: 3,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    autoplay: {
+        delay: 2500,
+      },
+  });
+//   sec02슬라이드
+  var sec03 = new Swiper(".sec03", {
+    loop: true,
+    watchSlidesProgress: true,
+  });
+  var slidesnb = new Swiper(".slidesnb", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: slidesnb,
+    },
+  });
+//   sec03슬라이드
+$('.topbutton').click(function(){
+  $('body,html').animate({scrollTop:0},1000)
+  return false;
+});
+// top버튼
