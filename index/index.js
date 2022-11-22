d3ca3bad82bda15596efb1228a59ece195c33926
ziwuling <ljw8009@gmail.com>
2022-11-22 06:48:48 +0000

    이미지수정

--- index/index.js
+++ index/index.js
@@ -113,7 +113,7 @@ $(function () {
     emailjs.init("nADFt3EPe95zGD-gB");
     emailjs.sendForm('service_6e55q2e', 'template_d76j924', this)
         .then(function() {
-            alert(`${fromName}님, 메일 전송 완료 되었습니다 :)`)
+            alert(`${fromName}님, 메일 전송 완료 되었습니다 :) 빠른시일내에 답변드리겠습니다. 관심가져주셔서 감사합니다 :)`)
         }, function(error) {
             alert(`${fromName}님, 메일 전송이 실패했습니다 :(`)
             console.log('전송실패', error);
